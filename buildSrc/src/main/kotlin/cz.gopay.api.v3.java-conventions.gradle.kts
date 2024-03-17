127ea22f8a03a6281aa4ab1700cd1eee1a7850e3
kyngs <kyngs@users.noreply.github.com>
2024-03-17 14:03:41 +0100

    Add publishing and CI

--- buildSrc/src/main/kotlin/cz.gopay.api.v3.java-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/cz.gopay.api.v3.java-conventions.gradle.kts
@@ -21,17 +21,39 @@ repositories {
 }
 
 group = "cz.gopay"
-version = "3.7.2"
-java.sourceCompatibility = JavaVersion.VERSION_1_8
+version = "3.0.0-SNAPSHOT"
+
+java {
+    withSourcesJar()
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
 
 publishing {
-    publications.create<MavenPublication>("maven") {
-        from(components["java"])
+    repositories {
+        maven {
+            name = "kyngsRepo"
+            url = uri(
+                "https://repo.kyngs.xyz/" + (if (project.version.toString()
+                        .contains("SNAPSHOT")
+                ) "snapshots" else "releases") + "/"
+            )
+            credentials(PasswordCredentials::class)
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
     }
 }
 
-tasks.withType<JavaCompile>() {
+tasks.withType<JavaCompile> {
     options.encoding = "UTF-8"
+    options.compilerArgs.add("-parameters")
 }
 
 tasks.withType<Javadoc>() {
